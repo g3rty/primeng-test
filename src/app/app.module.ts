b5630ce6627a00310cfd7df35780f821b01b5530
g3rty <amuee@web.de>
2022-11-04 08:08:46 +0100

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { AppComponent } from './app.component';
 import { TableModule } from 'primeng/table';
 import { TableComponent } from './table.component';
 import { TableWrapperComponent } from './wrapper/table-wrapper.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, TableModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    TableModule,
+    HttpClientModule,
+    // MatInputModule,
+  ],
   declarations: [AppComponent, TableWrapperComponent, TableComponent],
   bootstrap: [AppComponent],
 })
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+})
+export class SearchComponent implements OnInit {
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {}
+
+  getValue(event) {
+    console.log(event);
+  }
+}
